94f59567fae75a5b942b8720f7bec8bf38e2aa02
Karthik Vadlamudi <vkc427@gmail.com>
2021-05-20 14:25:50 -0500

    network checl

--- SeatGeek/Model/EventService.swift
+++ SeatGeek/Model/EventService.swift
@@ -10,25 +10,29 @@ import UIKit
 
 class EventService {
     func getFullEventInfo(completion: @escaping (Result<Events, Error>) -> Void) {
-        guard let url = URL(string: "\(EventURLs.kAPI_EventAPI)\(Auth.ClientID)") else {
-            return
+        if Reachability.isConnectedToNetwork() {
+            guard let url = URL(string: "\(EventURLs.kAPI_EventAPI)\(Auth.ClientID)") else {
+                return
+            }
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error.localizedDescription as! Error))
+                }
+                if let data = data {
+                    let jsonDecoder = JSONDecoder()
+                        do {
+                            let parsedJSON = try jsonDecoder.decode(Events.self, from: data)
+                            completion(.success(parsedJSON))
+                            
+                        } catch {
+                            print(error)
+                        }
+                }
+            }.resume()
+        } else {
+            //can display a error message. 
         }
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error.localizedDescription as! Error))
-            }
-            if let data = data {
-                let jsonDecoder = JSONDecoder()
-                    do {
-                        let parsedJSON = try jsonDecoder.decode(Events.self, from: data)
-                        completion(.success(parsedJSON))
-                        
-                    } catch {
-                        print(error)
-                    }
-            }
-        }.resume()
-        
+       
     }
     
 }
